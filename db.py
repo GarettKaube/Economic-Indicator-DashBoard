ad1ce48a7f9f36ce16eeb768ff744bee70d4a322
Garett Kaube <108091263+GarettKaube@users.noreply.github.com>
2023-05-28 18:03:29 -0600

    Update db.py

--- db.py
+++ db.py
@@ -9,15 +9,10 @@ import datetime
 st.set_page_config(layout="wide")
 
 # innitialization
-API_KEY = '9a048a276f1a939a1e64c77f214e5684'
+API_KEY = '#####' # MUST BE ADDED. Obtained from FRED 
 fred = Fred(api_key=API_KEY)
 series_header_size = 6
 
-
-def add_to_class(Class): #@save
-    def wrapper(obj):
-        setattr(Class, obj.__name__, obj)
-    return wrapper
 
 
 class Data:
@@ -35,13 +30,14 @@ class Data:
         self.data = None
         self.last_vals = None
 
+
     
 @add_to_class(Data)
 def get_data(self):
     try:
         data = fred.get_series(self.series)
     except Exception:
-        pass
+        st.write('\"{}\" is not available.'.format(self.name))
     else:
         self.data = add_names(data, self.name)
         if self.period == None:
@@ -50,41 +46,42 @@ def get_data(self):
         
 
 
-
 @add_to_class(Data)
 def write_data(self, chart_type = 'line'):
-        st.write("""
-        {} {}
-        """.format('#'*series_header_size, self.name))
-        
-        data_filtered = self.data[(self.data.index >= self.start) & (self.data.index <= self.end)]
-        # Handeling the case of a small time frame selection causing some series of small frequency not plotting
-        if data_filtered.size > 10:
-            if chart_type =='line':
-                st.line_chart(self.data)
+        if self.data is not None:
+            st.write("""
+            {} {}
+            """.format('#'*series_header_size, self.name))
+            
+            data_filtered = self.data[(self.data.index >= self.start) & (self.data.index <= self.end)]
+            # Handeling the case of a small time frame selection causing some series of small frequency not plotting
+            if data_filtered.size > 10:
+                if chart_type =='line':
+                    st.line_chart(data_filtered)
 
-            elif chart_type == 'area':
-                st.area_chart(self.data.fillna(method='ffill'))
-        
-        # if series is small plot the 4 last observed values
-        elif self.last_vals is not None and data_filtered.size > 1:
-            if chart_type =='line':
-                st.line_chart(data_filtered)
+                elif chart_type == 'area':
+                    st.area_chart(data_filtered.fillna(method='ffill'))
+            
+            # if series is small plot the 4 last observed values
+            elif self.last_vals is not None and data_filtered.size > 1:
+                if chart_type =='line':
+                    st.line_chart(data_filtered)
 
-            elif chart_type == 'area':
-                st.area_chart(data_filtered.fillna(method='ffill'))
-        
-        else:
-            st.bar_chart(self.last_vals.tail(2))
+                elif chart_type == 'area':
+                    st.area_chart(data_filtered.fillna(method='ffill'))
+            
+            else:
+                st.bar_chart(self.last_vals.tail(2))
 
 
 @add_to_class(Data)
 def write_latest_vals(self,n=4):
-        latest = display_most_recent(self.data, n)
-        st.write("""
-        ##### LATEST {} Values
-            """.format(self.name))
-        st.write(latest)
+        if self.data is not None:
+            latest = self.data.tail(n)
+            st.write("""
+            ##### LATEST {} Values
+                """.format(self.name))
+            st.write(latest)
 
 
 @add_to_class(Data)
@@ -94,19 +91,20 @@ def get_series(self):
 
 @add_to_class(Data)
 def calculate_pct_chg(self):
-     period = self.period * 12 if self.annual == True else self.period
+     if self.data is not None:
+        period = self.period * 12 if self.annual == True else self.period
 
-     self.percent_chg = ((self.data.diff(period)/ self.data.abs().shift(period))*100).dropna()
-     
-     percent_chg_filtered = self.percent_chg[(self.percent_chg.index <= self.end) & (self.percent_chg.index >= self.start)]
-     st.write("""
-        {} Percent Change for {}
-     """.format('#'*series_header_size, self.name))
-     if percent_chg_filtered.size > 1:
-       
-        st.line_chart(percent_chg_filtered)
-     else:
-         st.write("""### Not enough data to calculate percent change""")
+        self.percent_chg = ((self.data.diff(period)/ self.data.abs().shift(period))*100).dropna()
+        
+        percent_chg_filtered = self.percent_chg[(self.percent_chg.index <= self.end) & (self.percent_chg.index >= self.start)]
+        st.write("""
+            {} Percent Change for {}
+        """.format('#'*series_header_size, self.name))
+        if percent_chg_filtered.size > 1:
+        
+            st.line_chart(percent_chg_filtered)
+        else:
+            st.write("""### Not enough data to calculate percent change""")
 
 
 st.sidebar.header('Economic Indicator Selection')
@@ -118,11 +116,12 @@ with open('saved_series.pkl', 'rb') as f:
 
 @add_to_class(Data)
 def infer_freq(self):
-     freq = pd.infer_freq(self.data.index)
-     if freq == "QS-OCT":
-          self.period = 4
-     else:
-          self.period = 1
+     if self.data is not None:
+        freq = pd.infer_freq(self.data.index)
+        if freq == "QS-OCT":
+            self.period = 4
+        else:
+            self.period = 1
 
 
 
@@ -130,11 +129,12 @@ def infer_freq(self):
 selected_indicators = st.sidebar.multiselect('Indicator', fred_dict.keys())
 
 # Time frame selection
+#
 with st.sidebar:
-    start_select = st.date_input('Start of Time Series', min_value=datetime.date(1950,1,1), value =datetime.date(2014,1,1))
+    start_select = st.date_input('Start of Time Series', value =datetime.date(2014,1,1), min_value=datetime.date(1960,1,1))
     start = pd.Timestamp(start_select.year, start_select.month, start_select.day)
 
-    end_select = st.date_input('End of Time Series',min_value=datetime.date(1950,1,1) )
+    end_select = st.date_input('End of Time Series', min_value=datetime.date(1960,1,1))
     end = pd.Timestamp(end_select.year, end_select.month, end_select.day)
 
     # make sure the start date is before the end date
@@ -154,11 +154,26 @@ with st.sidebar:
         name_of_series = st.text_input("Name of data","Enter Name")
         annual = st.checkbox('If monthly data: Calculate percentage annualy?')
         submitted = st.form_submit_button("ADD")
-        
+
+
+    
         if submitted:
-            fred_dict[name_of_series] = [new_data, None, annual]
-            with open('saved_series.pkl', 'wb') as f:
-                pickle.dump(fred_dict, f)
+
+            check_for_FRED_Code = new_data.replace(" ", "") == "" or new_data == "FRED CODE"
+            check_for_name = name_of_series.replace(" ", "") == "" or name_of_series == "Enter Name"
+
+            if check_for_FRED_Code:
+                st.markdown(":red[Enter the FRED code for the Time Series!]")
+                if check_for_name:
+                    st.markdown(":red[Enter a name for the Time Series!]")
+
+            elif check_for_name and not check_for_FRED_Code :
+                st.markdown(":red[Enter a name for the Time Series!]")
+
+            else:
+                fred_dict[name_of_series] = [new_data, None, annual]
+                with open('saved_series.pkl', 'wb') as f:
+                    pickle.dump(fred_dict, f)    
 
 # Remove data 
 with st.sidebar:
