6447813a3dd7132e05db5a9b639af0691dcbc297
Garett Kaube <108091263+GarettKaube@users.noreply.github.com>
2023-08-29 14:05:51 -0600

    Update db.py
    clean up and optimization

--- db.py
+++ db.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import pandas as pd
 from fredapi import Fred
@@ -5,15 +6,17 @@ import streamlit as st
 from utils import *
 import pickle
 import datetime
+import plotly.graph_objects as go
+import plotly.express as px
+import time
+
 
 st.set_page_config(layout="wide")
 
 # innitialization
-API_KEY = '#######' # MUST BE ADDED. Obtained from FRED 
+API_KEY = '' # MUST BE ADDED. Obtained from FRED 
 fred = Fred(api_key=API_KEY)
 series_header_size = 6
-
-
 
 class Data:
     def __init__(self, series:str, name:str, start, period:int, end, annual = None):
@@ -26,60 +29,68 @@ class Data:
         self.period = period
         self.percent_chg = None
         self.annual = annual
-
+        
         self.data = None
-        self.last_vals = None
+        
 
 
-    
 @add_to_class(Data)
 def get_data(self):
+
     try:
-        data = fred.get_series_latest_release(self.series)
-        
+        data = pd.read_csv(f"./data/{self.name}.csv",index_col=0)
+        data.index = pd.to_datetime(data.index)
     except Exception:
-        st.write('\"{}\" is not available.'.format(self.name))
+        try:
+            data = fred.get_series_latest_release(self.series)
+            data.to_csv(f"./data/{self.name}.csv")
+        except Exception:
+            st.write('\"{}\" is not available.'.format(self.name))
     else:
+        
         self.data = add_names(data, self.name[:17])
         if self.period == None:
             self.infer_freq()
         self.last_vals = self.data.tail(4)
         
 
+    
 @add_to_class(Data)
 def write_data(self, chart_type = 'line'):
+        """
+        Lets user use the date slider to select date end points then plots the time series. Can plot line or area charts.
+        
+        """
         if self.data is not None:
+            fig = go.Figure()
+
             date_interval = st.slider("Date range", 
                                       value=(datetime.date(self.start.year, self.start.month, self.start.day), datetime.date(self.end.year, self.end.month, self.end.day)),
                                       key=self.name)
+            
             self.start = pd.Timestamp(date_interval[0].year, date_interval[0].month, date_interval[0].day)
             self.end = pd.Timestamp(date_interval[1].year, date_interval[1].month, date_interval[1].day)
+
             st.write("""
-            {} {}
-            """.format('#'*series_header_size, self.name))
+                    {} {}
+                     """.format('#'*series_header_size, self.name)
+                    )
             
             data_filtered = self.data[(self.data.index >= self.start) & (self.data.index <= self.end)]
-            # Handeling the case of a small time frame selection causing some series of small frequency not plotting
-            if data_filtered.size > 10:
-                if chart_type =='line':
-                    st.line_chart(data_filtered)
-
-                elif chart_type == 'area':
-                    st.area_chart(data_filtered.fillna(method='ffill'))
+            data_filtered_df = data_filtered.reset_index()
             
-            # if series is small plot the 4 last observed values
-            elif self.last_vals is not None and data_filtered.size > 1:
-                if chart_type =='line':
-                    st.line_chart(data_filtered)
-
-                elif chart_type == 'area':
-                    st.area_chart(data_filtered.fillna(method='ffill'))
+            if chart_type =='line':
+                fig.add_trace(go.Scatter(
+                    x=data_filtered_df['Date'],
+                    y = data_filtered_df[data_filtered_df.columns[1]]
+                ))
             
-            else:
-                st.bar_chart(self.last_vals.tail(2))
+            elif chart_type == 'area':
+                fig = px.area(data_filtered_df.ffill(), x='Date', y=data_filtered_df.columns[1])
+
+            st.plotly_chart(fig, use_container_width=True)
 
             
-
 
 @add_to_class(Data)
 def write_latest_vals(self,n=4):
@@ -115,8 +126,6 @@ def calculate_pct_chg(self):
             st.write("""### Not enough data to calculate percent change""")
 
 
-st.sidebar.header('Economic Indicator Selection')
-
 
 # Dictionary that is of the form: "Series name": ["Fred Code", Period]
 with open('saved_series.pkl', 'rb') as f:
@@ -132,191 +141,166 @@ def infer_freq(self):
             self.period = 1
 
 
-# selection of indicators 
-selected_indicators = st.sidebar.multiselect('Indicator', fred_dict.keys())
-num_indicators = len(selected_indicators)
-# Time frame selection
-#
-with st.sidebar:
-    start_select = st.date_input('Start of Time Series', value =datetime.date(2014,1,1), min_value=datetime.date(1960,1,1))
-    start = pd.Timestamp(start_select.year, start_select.month, start_select.day)
+class SideBar:
+    def __init__(self) -> None:
+        st.sidebar.header('Economic Indicator Selection')
+        self.selected_indicators = st.sidebar.multiselect('Indicator', fred_dict.keys())
+        self.num_indicators = len(self.selected_indicators)
+        self.start = None
+        self.end = None
+        self.stop = None
+    def select_data(self):
+        with st.sidebar:
+            start_select = st.date_input('Start of Time Series', value =datetime.date(2014,1,1), min_value=datetime.date(1960,1,1))
+            self.start = pd.Timestamp(start_select.year, start_select.month, start_select.day)
 
-    end_select = st.date_input('End of Time Series', min_value=datetime.date(1960,1,1))
-    end = pd.Timestamp(end_select.year, end_select.month, end_select.day)
+            end_select = st.date_input('End of Time Series', min_value=datetime.date(1960,1,1))
+            self.end = pd.Timestamp(end_select.year, end_select.month, end_select.day)
 
-    # make sure the start date is before the end date
-    try:
-        assert end >= start
-    except AssertionError:
-        st.markdown(":red[Start should be before end date!]")
-        stop = True
-    else:
-        stop = False
-
-
-# Get more data from FRED
-with st.sidebar:
-    with st.form('FREDData'):
-        new_data = st.text_input("Add FRED Data","FRED CODE")
-        name_of_series = st.text_input("Name of data","Enter Name")
-        annual = st.checkbox('If monthly data: Calculate percentage annualy?')
-        submitted = st.form_submit_button("ADD")
-
-        if submitted:
-
-            check_for_FRED_Code = new_data.replace(" ", "") == "" or new_data == "FRED CODE"
-            check_for_name = name_of_series.replace(" ", "") == "" or name_of_series == "Enter Name"
-
-            if check_for_FRED_Code:
-                st.markdown(":red[Enter the FRED code for the Time Series!]")
-                if check_for_name:
-                    st.markdown(":red[Enter a name for the Time Series!]")
-
-            elif check_for_name and not check_for_FRED_Code :
-                st.markdown(":red[Enter a name for the Time Series!]")
-
+            # make sure the start date is before the end date
+            try:
+                assert self.end >= self.start
+            except AssertionError:
+                st.markdown(":red[Start should be before end date!]")
+                self.stop = True
             else:
-                fred_dict[name_of_series] = [new_data, None, annual]
-                with open('saved_series.pkl', 'wb') as f:
-                    pickle.dump(fred_dict, f)    
-
-
-# Remove data 
-with st.sidebar:
-    with st.form('Remove Data'):
-        data_to_be_removed = st.multiselect('Remove Data From Database', fred_dict.keys())
-        submitted = st.form_submit_button("Remove")
-        if submitted:
-            for item in data_to_be_removed:
-                fred_dict.pop(item)
-            with open('saved_series.pkl', 'wb') as f:
-                pickle.dump(fred_dict, f)
-
-
-if num_indicators > 0:
-    tab1, tab2, tab3, tab4 = st.tabs(['Time-Series', 'Percent Change', 'Data', 'Summary'])
-elif  num_indicators == 1:
-    tab1, tab2, tab3 = st.tabs(['Time-Series', 'Percent Change', 'Data'])
-    tab4 = None
-else: 
-    tab1 = None
-    tab2 = None
-    tab3 = None
-    tab4 = None
-    st.text_area(label="Get Started", value = "Select a time series on the left", height=30)
-
-
-series_objects = []    # store series (Data) objects 
-
-num_indicators = len(selected_indicators)
-# Time-series tab
-if tab1 is not None:
-    with tab1:
-        # add select chart drop down menu when there is at least one time series selected
-        chart_type = st.selectbox('Chart Type', ('line', 'area'))
-
-        # set number of columns depending on how many time-series are selected
-        if num_indicators >= 2:
-            columns = st.columns(2, gap = 'medium')
-            n_cols = 2
-        else:
-            columns = st.columns(1, gap = 'medium')
-            n_cols = 1
-
-        # make series objects and display time plots of selected series
-
-        if not stop:
-            for i, ind in enumerate(selected_indicators):
-                
-                    with columns[i%n_cols]:
-                        try:
-                            series = Data(fred_dict[ind][0], ind, start, fred_dict[ind][1], end, fred_dict[ind][2])
-                        except Exception:
-                            try:
-                                series = Data(fred_dict[ind][0], ind, start, fred_dict[ind][1], end)
-                            except Exception:
-                                pass
-
-                        series.get_data()
-                        series_objects.append(series)
-                        series.write_data(chart_type)
-
-
-selected_indicators_for_pct = selected_indicators.copy()
-num_indicators_for_pct = len(selected_indicators_for_pct)
-
-# calculate and display percentage change of selected time series on second tab
-if tab2 is not None:
-    with tab2:
-        num_indicators_for_pct = len(selected_indicators_for_pct)
-
-        if num_indicators >= 2:
-            columns_pct = st.columns(2, gap = 'medium')
-            n_cols_pct = 2
-        else:
-            columns_pct = st.columns(1, gap = 'medium')
-            n_cols_pct = 1
-
-        for i,s in enumerate(series_objects):
-            with columns_pct[i%n_cols_pct]:
-                if s.name in selected_indicators_for_pct:
-                    s.calculate_pct_chg()
-
-
-# tab for showing latest values of selected time-series
-if tab3 is not None:
-    with tab3:
-        max = st.number_input("Maximum amount of latest values to display:", min_value=1, max_value=20, value=4)
-        columns_tab3 = st.columns(5, gap='medium')
-        for i in range(1,6):
-            if num_indicators == i:
-                columns_tab3 = st.columns(i, gap='medium')   
-
-        for i,s in enumerate(series_objects):
-            with columns_tab3[i%5]:
-                s.write_latest_vals(max)
-
-
-# correlations and summary statistics
-if tab4 is not None: 
-    with tab4:
-        if num_indicators > 1:
-            st.write("""### Correlation Matices""")
-            columns_tab4 = st.columns(4, gap='medium') 
-            if num_indicators in range(1,5):
-                columns_tab4 = st.columns(num_indicators, gap='medium') 
-
-            series_objects_copy = series_objects.copy()
-
-            k=0
-            j=1
-            for series in series_objects_copy[:-1]:
-                
-                for i in range(j, len(series_objects_copy)-1):
-                    if series.name != series_objects_copy[i].name:
-            
-                        corr_ = correlate(series.get_series(), series_objects_copy[i].get_series())
-                        
-                        
-                        with columns_tab4[k % np.min([4,num_indicators])]:
-                            st.write(corr_)
-                        k += 1
-                j += 1
-
-                
-
-        merged_series = series_objects[0].get_series()
-
-        if num_indicators > 1:
-            merged_series = pd.merge(series_objects[0].get_series(), series_objects[1].get_series(), on = 'Date')
-            for n in range(2, num_indicators):
-
-                merged_series = pd.merge(merged_series, series_objects[n].get_series(), on = 'Date')
+                self.stop = False
         
-        st.write("""### Summary Statistics""")
-        st.write(merged_series.describe())
+    def get_more_data(self):
+        # Get more data from FRED
+        with st.sidebar:
+            with st.form('FREDData'):
+                new_data = st.text_input("Add FRED Data","FRED CODE")
+                name_of_series = st.text_input("Name of data","Enter Name")
+                annual = st.checkbox('If monthly data: Calculate percentage annualy?')
+                submitted = st.form_submit_button("ADD")
+
+                if submitted:
+
+                    check_for_FRED_Code = new_data.replace(" ", "") == "" or new_data == "FRED CODE"
+                    check_for_name = name_of_series.replace(" ", "") == "" or name_of_series == "Enter Name"
+
+                    if check_for_FRED_Code:
+                        st.markdown(":red[Enter the FRED code for the Time Series!]")
+                        if check_for_name:
+                            st.markdown(":red[Enter a name for the Time Series!]")
+
+                    elif check_for_name and not check_for_FRED_Code :
+                        st.markdown(":red[Enter a name for the Time Series!]")
+
+                    else:
+                        fred_dict[name_of_series] = [new_data, None, annual]
+                        with open('saved_series.pkl', 'wb') as f:
+                            pickle.dump(fred_dict, f)    
+
+    def remove_data(self):
+        # Remove data 
+        with st.sidebar:
+            with st.form('Remove Data'):
+                data_to_be_removed = st.multiselect('Remove Data From Database', fred_dict.keys())
+                submitted = st.form_submit_button("Remove")
+                if submitted:
+                    for item in data_to_be_removed:
+                        fred_dict.pop(item)
+                    with open('saved_series.pkl', 'wb') as f:
+                        pickle.dump(fred_dict, f)
 
 
 
+class TimeSeriesTab(SideBar):
+    def __init__(self, tab = None) -> None:
+        self.series_objects = []    # store series (Data) objects 
+        self.tab = tab
+        super(TimeSeriesTab, self).__init__()
+
+    def runtab(self):
+        # Time-series tab
+        if self.tab is not None:
+            with self.tab:
+                # add select chart drop down menu when there is at least one time series selected
+                charttype = st.selectbox('Chart Type', ('line', 'area'))
+
+                # set number of columns depending on how many time-series are selected
+                if self.num_indicators >= 2:
+                    columns = st.columns(2, gap = 'medium')
+                    ncols = 2
+                else:
+                    columns = st.columns(1, gap = 'medium')
+                    ncols = 1
+
+                # make series objects and display time plots of selected series
+
+                if not self.stop:
+                    self.make_plots(columns, ncols, charttype)
 
 
+    def make_plots(self, columns, ncols, charttype):
+        """
+        Gets data and plots. Creates more streamlit columns as more time-series is added
+        """
+        for i, ind in enumerate(self.selected_indicators):
+            with columns[i%ncols]:
+                try:
+                    series = Data(fred_dict[ind][0], ind, self.start, fred_dict[ind][1], self.end, fred_dict[ind][2])
+                    print('yo')
+                except Exception:
+                    try:
+                        series = Data(fred_dict[ind][0], ind, self.start, fred_dict[ind][1], self.end)
+                    except Exception:
+                        pass
+                series.get_data()
+                self.series_objects.append(series)
+                series.write_data(charttype)
+
+
+
+def percent_change_tab(selected_indicators, num_indicators, series_objects, tab=None):
+    """
+    Plots the percent change of the selected time-series.
+    """
+    if tab is not None:
+        with tab:
+
+            if num_indicators >= 2:
+                columns_pct = st.columns(2, gap = 'medium')
+                n_cols_pct = 2
+            else:
+                columns_pct = st.columns(1, gap = 'medium')
+                n_cols_pct = 1
+
+            for i,s in enumerate(series_objects):
+                with columns_pct[i%n_cols_pct]:
+                    if s.name in selected_indicators:
+                        s.calculate_pct_chg()
+
+    
+
+
+def main():
+    
+    time_series = TimeSeriesTab()
+    time_series.select_data()
+    time_series.get_more_data()
+    time_series.remove_data()
+    print(time_series.num_indicators)
+
+   
+    if time_series.num_indicators > 1:
+        tab1, tab2, tab3, tab4 = st.tabs(['Time-Series', 'Percent Change', 'Data', 'Summary'])
+        time_series.tab = tab1
+    elif  time_series.num_indicators == 1:
+        tab1, tab2, tab3 = st.tabs(['Time-Series', 'Percent Change', 'Data'])
+        tab4 = None
+        time_series.tab = tab1
+    else: 
+        tab1 = None
+        tab2 = None
+        tab3 = None
+        tab4 = None
+        st.text_area(label="Get Started", value = "Select a time series on the left", height=30)
+    
+    time_series.runtab()
+    percent_change_tab(time_series.selected_indicators, time_series.num_indicators, time_series.series_objects, tab2)
+    
+
+main()
